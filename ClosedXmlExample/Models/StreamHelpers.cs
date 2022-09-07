fd1702c1493ae6452469cda93c85c0e138eea162
AchikoPapiashvili <Archilipapiashvili@gmail.com>
2022-09-07 06:20:58 -0700

    Add project files.

---
+++ ClosedXmlExample/Models/StreamHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosedXmlExample.Models
+{
+    public static class StreamHelpers
+    {
+        public static byte[] GetBytes(MemoryStream fs)
+        {
+            //MemoryStream fs = new MemoryStream();
+            TextWriter tx = new StreamWriter(fs);
+
+            tx.WriteLine("1111");
+            tx.WriteLine("2222");
+            tx.WriteLine("3333");
+
+            tx.Flush();
+            fs.Flush();
+
+            fs.Position = 0;
+
+            byte[] bytes = new byte[fs.Length];
+            fs.Read(bytes, 0, bytes.Length);
+
+            return bytes;
+        }
+    }
+}
---
+++ ClosedXmlExample/Program.cs
@@ -0,0 +1,46 @@
+﻿
+ using ClosedXML.Excel;
+using ClosedXmlExample.Models;
+
+List<Student> _oStudents = new List<Student>();
+
+
+   for (int i = 1; i <= 9; i++)
+   {
+        _oStudents.Add(new Student()
+        {
+           Id = i,
+           Name = "Student" + i,
+           Roll = "100" + i,
+           Address="test"+i
+        });
+    }
+
+
+
+using (var workbook = new XLWorkbook())
+{
+    var worksheet = workbook.Worksheets.Add("Student");
+    var currentRow = 1;
+
+    worksheet.Cell(currentRow, 1).Value = "StudentId";
+    worksheet.Cell(currentRow, 2).Value = "Name";
+    worksheet.Cell(currentRow, 3).Value = "Roll";
+    worksheet.Cell(currentRow, 4).Value = "Address";
+
+    foreach (var Student in _oStudents)
+    {
+        currentRow++;
+        worksheet.Cell(currentRow, 1).Value = Student.Id;
+        worksheet.Cell(currentRow, 2).Value = Student.Name;
+        worksheet.Cell(currentRow, 3).Value = Student.Roll;
+        worksheet.Cell(currentRow, 4).Value = Student.Address;
+    }
+
+    using (var stream = new MemoryStream())
+    {
+        workbook.SaveAs(stream);
+        File.WriteAllBytes("C:\\Users\\User\\Desktop\\test\\excel.xlsx", StreamHelpers.GetBytes(stream));
+    }
+}
+        Console.WriteLine("Hello, World!");
